ab5b2a726decf929c9e384444aaae6f2ed881423
Kyle Zaragoza <popwarsweet@gmail.com>
2017-02-24 17:02:38 -0800

    init commit

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,12 +1,15 @@
 import UIKit
 import XCTest
-import MessageDispatcher
+@testable import MessageDispatcher
 
 class Tests: XCTestCase {
+    
+    var messageDispatcher: MessageDispatcher<String>!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        messageDispatcher = MessageDispatcher<String>()
     }
     
     override func tearDown() {
@@ -14,16 +17,72 @@ class Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    func testMessageIsReceived() {
+        let expection = self.expectation(description: "Message is received by listener.")
+        let messageString = "Test message"
+        messageDispatcher.addEventListener(self, queue: nil) { message in
+            XCTAssertEqual(message, messageString)
+            expection.fulfill()
         }
+        messageDispatcher.alertListeners(messageString)
+        self.waitForExpectations(timeout: 0.1, handler: nil)
     }
     
+    func testReleasedListenerIsRemovedAfterMessageDispatch() {
+        var listener: NSObject? = NSObject()
+        messageDispatcher.addEventListener(listener!, queue: nil) { _ in }
+        listener = nil
+        messageDispatcher.alertListeners("Message")
+        XCTAssertEqual(messageDispatcher.listeners.count, 0)
+    }
+    
+    func testListenerIsWeaklyHeld() {
+        var listener: NSObject? = NSObject()
+        messageDispatcher.addEventListener(listener!, queue: nil) { _ in }
+        listener = nil
+        messageDispatcher.resize()
+        XCTAssertEqual(messageDispatcher.listeners.count, 0)
+    }
+    
+    func testMessageIsReceivedByMultipleListeners() {
+        let expectation = self.expectation(description: "Message is received by multiple listeners.")
+        let listenerOne = NSObject()
+        let listenerTwo = NSObject()
+        var numberOfMessagesReceived = 0
+        messageDispatcher.addEventListener(listenerOne, queue: nil) { message in
+            numberOfMessagesReceived += 1
+            if numberOfMessagesReceived == 2 {
+                expectation.fulfill()
+            }
+        }
+        messageDispatcher.addEventListener(listenerTwo, queue: nil) { message in
+            numberOfMessagesReceived += 1
+            if numberOfMessagesReceived == 2 {
+                expectation.fulfill()
+            }
+        }
+        messageDispatcher.alertListeners("Test message")
+        self.waitForExpectations(timeout: 0.1, handler: nil)
+    }
+    
+    func testListenerIsManuallyRemoved() {
+        let listener = NSObject()
+        messageDispatcher.addEventListener(listener, queue: nil) { message in }
+        messageDispatcher.removeEventListener(listener)
+        XCTAssertEqual(messageDispatcher.listeners.count, 0)
+    }
+    
+    func testListenerIsCalledOnCorrectQueue() {
+        let expectation = self.expectation(description: "Message is received on correct queue.")
+        let listener = NSObject()
+        let queue = DispatchQueue(label: "TestQueue")
+        let key = DispatchSpecificKey<Void>()
+        queue.setSpecific(key:key, value: ())
+        messageDispatcher.addEventListener(listener, queue: queue) { message in
+            guard DispatchQueue.getSpecific(key: key) != nil else { return }
+            expectation.fulfill()
+        }
+        messageDispatcher.alertListeners("Test message")
+        self.waitForExpectations(timeout: 0.1, handler: nil)
+    }
 }
---
+++ MessageDispatcher/Classes/MessageDispatcher.swift
@@ -0,0 +1,98 @@
+//
+//  MessageDispatcher.swift
+//
+//
+//  Created by Kyle Zaragoza on 2/17/17.
+//  Copyright © 2017 Kyle Zaragoza. All rights reserved.
+//
+
+import Foundation
+
+/// Contains the closure and queue that the message should be dispatched on.
+final public class HandlerWrapper<T> {
+    let handler: ((_ object: T) -> Void)
+    var queue: DispatchQueue?
+    init(handler: @escaping ((_ object: T) -> Void), queue: DispatchQueue? = nil) {
+        self.handler = handler
+        self.queue = queue
+    }
+}
+
+/// Used for weakly wrapping listeners.
+final public class WeakWrapper<T: AnyObject>: Hashable {
+    weak var value: T?
+    init (value: T) {
+        self.value = value
+    }
+    
+    public var hashValue: Int {
+        return ObjectIdentifier(self).hashValue
+    }
+    
+    public static func ==(lhs: WeakWrapper<T>, rhs: WeakWrapper<T>) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+}
+
+/// The class for managing event listeners as well as dispatching messages.
+final class MessageDispatcher<MessageType> {
+    typealias ListenerType = WeakWrapper<AnyObject>
+    typealias MessageHandler<T> = ((_ object: T) -> Void)
+    
+    /// Dictionary of listeners as keys, and wrapped handlers as their values.
+    /// - NOTE: Once a listener (key) is released, the table will not immediately be resized. Do not rely on the count of `listeners` to accurately reflect the number of listeners.
+    private(set) var listeners = [ListenerType: HandlerWrapper<MessageType>]()
+    
+    
+    /// Removes all keys that have nil values.
+    func resize() {
+        let nilListeners = Array(listeners.keys.filter { $0.value == nil })
+        nilListeners.forEach { self.listeners.removeValue(forKey: $0) }
+    }
+    
+    /// Alerts all listeners of the given message.
+    ///
+    /// - Parameter message: The message to send to the listeners.
+    func alertListeners(_ message: MessageType) {
+        var needsResizing = false
+        // Alert all non-nil listeners.
+        for (k, v) in listeners {
+            guard k.value != nil else {
+                // Flag that listeners needs resizing.
+                needsResizing = true
+                continue
+            }
+            if let queue = v.queue {
+                queue.async { v.handler(message) }
+            } else {
+                v.handler(message)
+            }
+        }
+        // Resize if need be.
+        if needsResizing { resize() }
+    }
+    
+    /// Adds a closure to be called when messages are dispatched.
+    ///
+    /// - Parameters:
+    ///   - listener: The object which is listening to messagse, this object is weakly held.
+    ///   - queue: The queue to have the handler called on, specify nil to be called on the calling queue.
+    ///   - handler: The closure to be called when the dispatcher alerts all listeners.
+    func addEventListener(_ listener: AnyObject, queue: DispatchQueue?, handler: @escaping MessageHandler<MessageType>) {
+        // wrap in a class so we can store it in a MapTable
+        let handlerWrapper = HandlerWrapper<MessageType>(handler: handler, queue: queue)
+        let weakListener = WeakWrapper(value: listener)
+        listeners[weakListener] = handlerWrapper
+    }
+    
+    /// Removes a listener from the dispatcher
+    ///
+    /// - Parameter listener: This object to be removed.
+    func removeEventListener(_ listener: AnyObject) {
+        listeners.keys.filter {
+            $0.value === listener
+        }.forEach {
+            listeners[$0] = nil
+        }
+    }
+}
